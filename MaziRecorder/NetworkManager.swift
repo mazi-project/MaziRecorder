1572909cde2605fbe4b864ef4fac79ae4c39cbc5
lutzer <t@t.t>
2016-07-09 15:54:42 +0200

    added toto list and created network manager

---
+++ MaziRecorder/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  MaziRecorder
+//
+//  Created by Lutz on 09/07/16.
+//  Copyright © 2016 Erich Grunewald. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    let interview : Interview
+    
+    init(interview: Interview) {
+        self.interview = interview
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MaziRecorder
+
+## TODO
+
+* [ ] Load persisted state
+* [ ] Create Network manager
+* [ ] Styling
+* [ ] Reseting Interview
+* [ ] Color questions depening on if they are answered
+* [ ] Upload View
+* [ ] Fix Label Text
+* [ ] Sound Visualisation for Audio Recorder
